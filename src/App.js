0e061ed5b0069909ff94bb656de22009c85060e4
ArmaniKE <armani.kabidolda.18.12.03@gmail.com>
2023-09-24 20:01:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Artist = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Album: {props.album}</h2>
+      <img src={props.img} alt="img" />
+    </>
+  );
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = `Counter: ${counter}`;
+  }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Artist 
+        name={"Travis Scott"} 
+        album={"Utopia"} 
+        img={'https://pbs.twimg.com/profile_images/634514155261833216/czgYrPLQ_400x400.jpg'} 
+      /><br></br>
+      <button onClick={() => setCounter(prevCount => prevCount + 1)}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(prevCount => prevCount - 1)}>-</button>
     </div>
   );
 }
